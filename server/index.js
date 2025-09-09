b8f72d86445479db9ff601670cf6f1829f60bc12
asadshakri <asadshakri3127@gmail.com>
2025-09-09 18:40:03 +0530

    middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ const app=express();
 
 const PORT=3000;
 
+app.use('/welcome',(req,res,next)=>{
+    req.user="Guest";
+    next();
+})
+
+app.get('/welcome',(req,res)=>{
+    res.send(`<h1>Welcome, ${req.user}<h1>`);
+});
+
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
 });
