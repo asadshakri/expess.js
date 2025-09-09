1d73ebac29c918c9f383d42c91cf3345186156c5
asadshakri <asadshakri3127@gmail.com>
2025-09-09 20:20:03 +0530

    random routes

--- routes/app.js
+++ routes/app.js
@@ -1,22 +1,25 @@
 const express=require("express");
 const app=express();
-const port=3000;
+const port=4000;
 
-app.get('/orders',(req,res)=>{
-    res.send("here is the list of all orders");
+app.get('/products',(req,res)=>{
+    res.send("here is the list of all products");
 });
 
-app.post('/orders',(req,res)=>{ 
-    res.send(" A new order has been created");
+app.post('/products',(req,res)=>{ 
+    res.send(" A new product has been added");
 });
 
-app.get('/users',(req,res)=>{
-    res.send("here is the list of all users");
+app.get('/categories',(req,res)=>{
+    res.send("here is the list of all categories");
 });
-app.post('/users',(req,res)=>{
-    res.send("A new user has been added");
+app.post('/categories',(req,res)=>{
+    res.send("A new categories has been created");
 });
 
+app.all('/*splat',(req,res)=>{
+    res.status(404).send("<h1>404! Page Not Found</h1>");
+});
 
 app.listen(port,()=>{
     console.log(`Server is up and running on http://localhost:${port}`);
